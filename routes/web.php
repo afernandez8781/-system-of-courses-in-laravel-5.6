337cc547247299be4c4d3769a3c5e568b40a37df
Alfredo fernandez <afernandez8781@hotmail.com>
2018-11-12 16:59:35 -0400

    part 12 finished managing subscriptions and invoices with cashier and stripe

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,27 @@ Route::group(['prefix' => 'courses'], function () {
     Route::get('/{course}', 'CourseController@show')->name('courses.detail');
 });
 
-Route::group(['prefix' => 'subscriptions'], function () {
-	Route::get('/plans', 'SubscriptionController@plans')->name('subscriptions.plans');
-	
-	Route::get('/admin', 'SubscriptionController@admin')->name('subscriptions.admin');
+Route::group(['middleware' => ['auth']], function() {
 
-	Route::post('/process_subscription', 'SubscriptionController@processSubscription')
-		->name('subscriptions.process_subscription');
+    Route::group(['prefix' => 'subscriptions'], function () {
+    	Route::get('/plans', 'SubscriptionController@plans')->name('subscriptions.plans');
+    	
+    	Route::get('/admin', 'SubscriptionController@admin')->name('subscriptions.admin');
 
-    Route::post('/resume', 'SubscriptionController@resume')->name('subscriptions.resume');
-    Route::post('/cancel', 'SubscriptionController@cancel')->name('subscriptions.cancel');
+    	Route::post('/process_subscription', 'SubscriptionController@processSubscription')
+    		->name('subscriptions.process_subscription');
 
+        Route::post('/resume', 'SubscriptionController@resume')->name('subscriptions.resume');
+        Route::post('/cancel', 'SubscriptionController@cancel')->name('subscriptions.cancel');
+
+    });
+
+    Route::group(['prefix' => 'invoices'], function() {
+        Route::get('/admin', 'InvoiceController@admin')->name('invoices.admin');
+        Route::get('/{invoice}/download', 'InvoiceController@download')->name('invoices.download');
+    });
 });
+
 
 Route::get('/images/{path}/{attachment}', function($path, $attachment) {
     $file = sprintf('storage/%s/%s', $path, $attachment);
